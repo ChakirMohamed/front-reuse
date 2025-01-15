ecb0738e47e3c376197eaaeb9a56ba29a192fb2e
ChakirMohamed <chakir.mohamed8@ofppt-edu.ma>
2025-01-15 11:52:25 +0100

    config sidebar without api create api service

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,17 +6,29 @@ export interface RouteInfo {
     title: string;
     icon: string;
     class: string;
+    children?: RouteInfo[]; // support for submenus
+    expanded?: boolean; // Optional property for tracking submenu state
+
 }
 
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+    {
+      path: '/courriers',   title: 'Courriers',         icon:'nc-email-85',   class: '',
+      expanded: false,
+      children: [
+          { path: '/courriers/conventions',    title: 'Conventions',    icon: '', class: '' },
+          { path: '/courriers/demande-appui',  title: 'Demande d\'appui', icon: '', class: '' },
+          { path: '/courriers/autre',          title: 'Autre',          icon: '', class: '' },
+      ]
+    },
+    /*{ path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
     { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
     { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
     { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
     { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
     { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },*/
 ];
 
 @Component({
@@ -30,4 +42,7 @@ export class SidebarComponent implements OnInit {
     ngOnInit() {
         this.menuItems = ROUTES.filter(menuItem => menuItem);
     }
+    toggleSubmenu(menuItem: RouteInfo): void {
+      menuItem.expanded = !menuItem.expanded; // Toggle expanded state
+  }
 }
